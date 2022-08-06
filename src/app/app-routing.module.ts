65eaed43c149e7cef166193dc04d5f2684ea2016
Camiloss1 <juan@sbl.dev>
2022-08-06 12:48:34 -0500

    creando el proyecto inicial

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DefaultComponent } from './layouts/default/default.component';
+import { FullwidthComponent } from './layouts/fullwidth/fullwidth.component';
+import { ContactComponent } from './modules/contact/contact.component';
+import { HomeComponent } from './modules/home/home.component';
+import { LoginComponent } from './modules/login/login.component';
+
+const routes: Routes = [
+  {path:'', redirectTo:'full/login', pathMatch:'full'},
+  {
+    path: 'default', component: DefaultComponent,
+    children: [{
+      path: 'home',
+      component: HomeComponent
+    },
+    {
+      path: 'contact',
+      component: ContactComponent
+    }]
+  },
+  {
+    path: 'full', component: FullwidthComponent,
+    children: [{
+      path: 'login',
+      component: LoginComponent
+    }  
+  ]
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from './components/footer/footer.component';
+import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+
+
+
+@NgModule({
+  declarations: [
+    FooterComponent,
+    HeaderComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
+  exports:[
+    FooterComponent
+  ]
+})
+export class SharedModule { }
